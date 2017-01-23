b5e03f69566aac9dda6f3517fd4d523665321057
Evan Corl <evancorl@yahoo.com>
2017-01-22 20:11:26 -0600

    Ignore eslint on webpack dependencies

--- src/server/middleware/webpack/index.js
+++ src/server/middleware/webpack/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
